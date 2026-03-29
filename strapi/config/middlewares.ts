01e6ff32721d9d64ed58bf8fa9348d083e67a6b4
DMXPRO Dev <dev@dmxpro.com>
2026-03-29 17:10:42 -0300

    feat: agrega configuración de middlewares en strapi

---
+++ strapi/config/middlewares.ts
@@ -0,0 +1,22 @@
+import type { Core } from '@strapi/strapi';
+
+const config: Core.Config.Middlewares = [
+  'strapi::logger',
+  'strapi::errors',
+  'strapi::security',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['https://venetian.com.ar', 'https://www.venetian.com.ar'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+    },
+  },
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
+
+export default config;
