9a7ab8d66c71191d180f4d58fe85b120d2ff0f2b
DMXPRO Dev <dev@dmxpro.com>
2026-03-30 17:30:14 -0300

    feat: agrega controlador para productos

---
+++ strapi/src/api/producto/controllers/producto.js
@@ -0,0 +1,47 @@
+﻿'use strict';
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::producto.producto', ({ strapi }) => {
+  const productService = strapi.service('api::producto.producto');
+  const baseController = createCoreController('api::producto.producto');
+
+  return {
+    ...baseController,
+    async findFeatured(ctx) {
+      try {
+        const { query } = ctx;
+        const { results, pagination } = await productService.find({
+          ...query,
+          filters: {
+            ...query.filters,
+            featured: true,
+          },
+        });
+        return { data: results, meta: { pagination } };
+      } catch (error) {
+        ctx.throw(500, error);
+      }
+    },
+
+    async findByCategory(ctx) {
+      try {
+        const { categoriaId } = ctx.params;
+        if (!categoriaId) {
+          return ctx.badRequest('categoriaId es requerido');
+        }
+        const { query } = ctx;
+        const { results, pagination } = await productService.find({
+          ...query,
+          filters: {
+            ...query.filters,
+            category: categoriaId,
+          },
+        });
+        return { data: results, meta: { pagination } };
+      } catch (error) {
+        ctx.throw(500, error);
+      }
+    },
+  };
+});
