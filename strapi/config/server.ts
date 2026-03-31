b5df9abd0bbdcc371805bedbfdc3a623196eadb7
DMXPRO Dev <dev@dmxpro.com>
2026-03-30 21:05:32 -0300

    feat: agrega configuración del servidor en strapi

---
+++ strapi/config/server.ts
@@ -0,0 +1,18 @@
+import type { Core } from '@strapi/strapi';
+
+const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Server => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  app: {
+    keys: env.array('APP_KEYS'),
+  },
+  url: env('PUBLIC_URL', `http://${env('HOST', '0.0.0.0')}:${env.int('PORT', 1337)}`),
+  proxy: env.bool('PROXY', false),
+  cron: {
+    enabled: env.bool('CRON_ENABLED', false),
+  },
+
+
+});
+
+export default config;
